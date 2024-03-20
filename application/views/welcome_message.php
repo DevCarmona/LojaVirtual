29ce62327ed6f5a4bf34166f2c883d066b8ed14e
Andre da Silva Carmona <104648375+DevCarmona@users.noreply.github.com>
2024-03-20 11:24:51 -0300

    cmt

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Testing</title>
+	<title>LojaVirtual</title>
 
 	<style type="text/css">
 
