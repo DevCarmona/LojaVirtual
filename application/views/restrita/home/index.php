ad42b4f7c83ac14a0c1449f6304c75ee43c3a950
Andre da Silva Carmona <104648375+DevCarmona@users.noreply.github.com>
2024-03-21 11:06:06 -0300

    cmt

--- application/views/restrita/home/index.php
+++ application/views/restrita/home/index.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
-
-
-<!-- blank.html  21 Nov 2019 03:54:41 GMT -->
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Otika - Admin Dashboard Template</title>
+    <title>QRA-Store</title>
     <!-- General CSS Files -->
-    <link rel="stylesheet" href="<?php echo base_url('public/assets/css/app.min.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('/public/assets/css/app.min.css');?>">
     <!-- Template CSS -->
     <link rel="stylesheet" href="<?php echo base_url('public/assets/css/style.css');?>">
     <link rel="stylesheet" href="<?php echo base_url('public/assets/css/components.css');?>">
