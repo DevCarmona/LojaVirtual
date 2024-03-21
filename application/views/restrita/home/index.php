3c516b0c3c2e9fc97b289cb2d411038ad7f9d483
Andre da Silva Carmona <104648375+DevCarmona@users.noreply.github.com>
2024-03-21 10:37:35 -0300

    cmt

--- application/views/restrita/home/index.php
+++ application/views/restrita/home/index.php
@@ -9,13 +9,13 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>Otika - Admin Dashboard Template</title>
     <!-- General CSS Files -->
-    <link rel="stylesheet" href="assets/css/app.min.css">
+    <link rel="stylesheet" href="<?php echo base_url('public/assets/css/app.min.css');?>">
     <!-- Template CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/components.css">
+    <link rel="stylesheet" href="<?php echo base_url('public/assets/css/style.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('public/assets/css/components.css');?>">
     <!-- Custom style CSS -->
-    <link rel="stylesheet" href="assets/css/custom.css">
-    <link rel='shortcut icon' type='image/x-icon' href='assets/img/favicon.ico' />
+    <link rel="stylesheet" href="<?php echo base_url('public/assets/css/custom.css');?>">
+    <link rel='shortcut icon' type='image/x-icon' href="<?php echo base_url('public/assets/img/favicon.ico');?>" />
 </head>
 
 <body>
@@ -513,13 +513,13 @@
         </div>
     </div>
     <!-- General JS Scripts -->
-    <script src="assets/js/app.min.js"></script>
+    <script src="<?php echo base_url('public/assets/js/app.min.js');?>"></script>
     <!-- JS Libraies -->
     <!-- Page Specific JS File -->
     <!-- Template JS File -->
-    <script src="assets/js/scripts.js"></script>
+    <script src="<?php echo base_url('public/assets/js/scripts.js');?>"></script>
     <!-- Custom JS File -->
-    <script src="assets/js/custom.js"></script>
+    <script src="<?php echo base_url('public/assets/js/custom.js');?>"></script>
 </body>
 
 
